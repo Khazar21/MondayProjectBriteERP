d078dff4638d4dab20ed3ba5184b8413e25606a7
Khazar <khazarmahmudov@gmail.com>
2019-01-29 23:47:58 -0700

    added Invoicing Page AppConstants

---
+++ src/test/java/com/briteERP/utilities/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package com.briteERP.utilities;
+
+public class ApplicationConstants {
+    public static final String LOGIN_ERROR_MESSAGE= "Wrong login/password";
+    public static final String LOGIN_PAGE_TITLE= "Login | Website localhost";
+    public static final String CUSTOMER_INVOICES= "Customer Invoices";
+    public static final String CUSTOMER_CREDIT_NOTES= "Customer Credit Notes";
+    public static final String PAYMENTS= "Payments";
+}
